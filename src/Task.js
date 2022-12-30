ceace9c15a4bef0557c716de53414674e3382837
jesus bañuelos <jesusbg1225dic@gmail.com>
2022-12-29 20:10:46 -0600

    prueba

---
+++ src/Task.js
@@ -0,0 +1,71 @@
+import "./estilosSesion.css";
+import pesa from "./img/peso.png";
+import phone from "./img/phone.png";
+
+
+export function TarjetaSuperior() {
+  return (
+    /*Esta seccion retorna la parte superior de la pagina*/
+    <div className="cont-sup">
+      <h1>BodyFitmx</h1>
+      <img src={pesa} height="50px" className="img-register" />
+    </div>
+  );
+}
+
+export function ImgPhone() {
+  return (
+    <div className="cont-phone">
+      <img src={phone} width="500px" />
+    </div>
+  );
+}
+
+/*Esta funcion retorna la vista de iniciar sesion */
+export function TarjetaIniciarSesion() {
+  return (
+    <div className="cont-D-log" id="login-form">
+      <div className="cont-log">
+        <h2>Sesion</h2>
+        <input type="email" id="email-login" placeholder="Email" required />
+        <input
+          type="password"
+          id="password-login"
+          placeholder="Contraseña"
+          required
+        />
+        <button id="btn-Login">Iniciar Sesion</button>
+        <span>¿Olvidaste tu contraseña?</span>
+      </div>
+    </div>
+  );
+}
+
+/*Esta funcion retorna la vista de Registrar usuario */
+export function TarjetaRegistro() {
+  return (
+    <div className="cont-D-log" id="login-form">
+      <div className="cont-log">
+        <h2>Registro</h2>
+        <input type="text" placeholder="UserName"></input>
+        <input type="email" id="email-login" placeholder="Email" required />
+        <input
+          type="password"
+          id="password-login"
+          placeholder="Contraseña"
+          required
+        />
+        <input
+          type="password"
+          id="password-login"
+          placeholder="Confirmar Contraseña"
+          required
+        />
+        <button id="btn-Login">Registrase</button>
+        <span>¿Ya tienes una cuenta?</span>
+      </div>
+    </div>
+  );
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  TarjetaSuperior,
+  ImgPhone,
+  TarjetaIniciarSesion,
+  TarjetaRegistro,
+} from "./Task";
+import "./estilosSesion.css";
+import indice from "./img/indice.png";
+import { useState } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const [estado,nEstado] = useState( <TarjetaIniciarSesion />)
+
+function Elegir() {
+  return (
+    <div>
+     
+      <div className="cont-registrar">
+        <p>¿No tienes una cuenta?</p>
+        <span
+            onClick={() => {
+            nEstado(<TarjetaRegistro />) 
+            console.log("exito");
+          }}
+        >
+          Registrate
+        </span>
+      </div>
+    </div>
+  );
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <>
+    <TarjetaSuperior />
+    <ImgPhone />
+    <Elegir />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <div className="cont-descargas">
+      <img src={indice} width="150px" />
+    </div>
+    <footer>Todos los derechos son propiedad de BodyFit.com</footer>
+  </>
+);
